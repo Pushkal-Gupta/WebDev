8ce1d7cae962e71aee75eb99f7380887aeaeebe1
Pushkal-Gupta <pushkalgupta2005@gmail.com>
2026-08-11 10:01:36 +0530

    Workflow Bug fixes

--- PGHub/eslint.config.js
+++ PGHub/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'scripts/_*', 'docs/workflows/*']),
+  globalIgnores(['dist', 'scripts/_*', 'scripts/*.workflow.js', 'docs/workflows/*']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
